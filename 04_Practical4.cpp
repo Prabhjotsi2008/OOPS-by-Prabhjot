bd506711be21dd0eb719d2b44a7e6a02380f2d17
Prabhjotsi2008 <prabhjotsi2008@gmail.com>
2026-08-10 13:00:26 +0530

    completed practical 4

---
+++ 04_Practical4.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student{
+    string name;
+    int rollNo;
+    float marks[3];
+
+    float calcPercentage(){
+        float sum = 0;
+        for(int i=0; i<3; i++){
+            sum += marks[i];
+        }
+        float percent = (sum / (3 * 100)) * 100;
+        return percent;
+    }
+
+    public:
+        void getData(){
+            cout << "Enter Name: ";
+            cin >> name;
+            cout << "Enter Roll No: ";
+            cin >> rollNo;
+            for(int i=0; i<3; i++){
+                cout << "Enter Subject " << i+1 << " Marks: ";
+                cin >> marks[i];
+            }
+        }
+
+        void displayData(){
+            cout << "\nStudent's Details" << endl;
+            cout << "Name: " << name << endl;
+            cout << "Roll No: " << rollNo << endl;
+            cout << "Percentage: " << calcPercentage() << "%" << endl; // use of nested member function
+        }
+};
+
+int main() {
+    Student s1;
+    s1.getData();
+    s1.displayData();
+    return 0;
+}
